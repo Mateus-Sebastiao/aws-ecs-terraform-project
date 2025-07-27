1cb186e866e22e0e0c3b5e5557dce6134c0533cf
Mateus-Sebastião <mateussebastiao2003@gmail.com>
2025-07-27 16:56:44 +0100

    Update remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "meu-bucket-terraform-20250727"
     key            = "infra/producao/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
